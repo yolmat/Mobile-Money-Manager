5fbdbd537430a45195dff93e98d3bbfdebfa26f2
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-07-11 04:16:58 -0300

    Create page Create Profile

---
+++ app/CreateProfile/index.js
@@ -0,0 +1,28 @@
+import { Text, View, StyleSheet, Button } from "react-native";
+import { Link } from "expo-router";
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text >Create Screen</Text>
+            <Link href="/" push asChild>
+                <Button title="Push to Home" />
+            </Link>
+            <Link href="/CreateProfile" push asChild>
+                <Button title="Push to Create" />
+            </Link>
+            <Link href="/ListProfiles" push asChild>
+                <Button title="Push to List" />
+            </Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center'
+    }
+})
