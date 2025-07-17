f7d037dbc487fe34096a8e0c084ee90d5e791a1c
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-07-17 02:49:18 -0300

    update all screens

---
+++ app/CreateProfile.js
@@ -0,0 +1,34 @@
+import { Text, View, StyleSheet, Button } from "react-native";
+import { router } from "expo-router";
+
+export default function Home() {
+    function goToCreate() {
+        router.push("/CreateProfile")
+    }
+
+    function goToHome() {
+        router.push("/")
+    }
+
+    function goToList() {
+        router.push("/ListProfiles")
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text >Create Screen</Text>
+            <Button title="Push to Home" onPress={goToHome} />
+            <Button title="Push to Create" onPress={goToCreate} />
+            <Button title="Push to List" onPress={goToList} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center'
+    }
+})
--- app/index.js
+++ app/index.js
@@ -1,19 +1,25 @@
 import { Text, View, StyleSheet, Button } from "react-native";
-import { Link } from "expo-router";
+import { router } from "expo-router";
 
 export default function Home() {
+  function goToCreate() {
+    router.push("/CreateProfile")
+  }
+
+  function goToHome() {
+    router.push("/")
+  }
+
+  function goToList() {
+    router.push("/ListProfiles")
+  }
+
   return (
     <View style={styles.container}>
-      <Text >Index Screen</Text>
-      <Link href="/" push asChild>
-        <Button title="Push to Home" />
-      </Link>
-      <Link href="/CreateProfile" push asChild>
-        <Button title="Push to Create" />
-      </Link>
-      <Link href="/ListProfiles" push asChild>
-        <Button title="Push to List" />
-      </Link>
+      <Text >Create Screen</Text>
+      <Button title="Push to Home" onPress={goToHome} />
+      <Button title="Push to Create" onPress={goToCreate} />
+      <Button title="Push to List" onPress={goToList} />
     </View>
   );
 }
@@ -23,6 +29,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    textAlign: 'center',
-  },
+    textAlign: 'center'
+  }
 })
