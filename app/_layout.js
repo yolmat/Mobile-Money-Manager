635e5cc5be365b69aed778a657a6709891b2e5d0
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-07-17 02:48:58 -0300

    update Stack layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,22 @@
 import { Stack } from "expo-router";
-import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="auto" />
-      <Stack />
-    </>
+    <Stack
+      screenOptions={{
+        headerTitleAlign: "center",
+        headerStyle: {
+          backgroundColor: "#135456",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        }
+      }}
+    >
+      <Stack.Screen name="index" options={{ headerTitle: "Home" }} />
+      <Stack.Screen name="CreateProfile" options={{ headerTitle: "Criar Usuario" }} />
+      <Stack.Screen name="ListProfiles" options={{ headerTitle: "Lista de Usuarios" }} />
+    </Stack>
   )
 }
